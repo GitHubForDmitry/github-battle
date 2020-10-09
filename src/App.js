22ec01f7972d47be377a8f8596cce3e09bb45f19
Dmitry Koka <koka.d.wezom@gmail.com>
2020-10-09 09:37:57 +0300

    Webpack

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-
-    </div>
-
-  );
+class App extends React.Component {
+  render() {
+    return (
+        <h1>Hello</h1>
+    )
+  }
 }
 
 export default App;
